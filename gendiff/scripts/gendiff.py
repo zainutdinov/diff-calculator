2e295de20b02632ed4a6b711f71a3094181cf7f1
Анатолий Зайнутдинов <zainutdinov.aaa@gmail.com>
2024-07-27 18:23:57 +0300

    update deps and template

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+
+
+if __name__ == '__main__':
+    main()
